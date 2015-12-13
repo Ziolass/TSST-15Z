4f3acb050382a301664dbb136076625b0f9c3d12
Jan Sobczak <jasobeczek@gmail.com>
2015-12-13 18:52:44 +0100

    no message

--- NetworkNode/Program.cs
+++ NetworkNode/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using NetworkNode.SDHFrame;
 using NetworkNode.HPC;
 using System;
@@ -24,56 +23,3 @@ namespace NetworkNode
         }
     }
 }
-=======
-﻿using NetworkNode.SDHFrame;
-using NetworkNode.HPC;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NetworkNode
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Frame frame = new Frame(StmLevel.STM4);
-            frame.Msoh = new Header("test", "test2", "test");
-            frame.Rsoh = new Header("test", "test2", "test");
-           
-
-            frame.SetVirtualContainer(VirtualContainerLevel.VC4, 0, new VirtualContainer(VirtualContainerLevel.VC4));
-            frame.SetVirtualContainer(VirtualContainerLevel.VC4, 1, new VirtualContainer(VirtualContainerLevel.VC4));
-            frame.SetVirtualContainer(VirtualContainerLevel.VC4, 2, new VirtualContainer(VirtualContainerLevel.VC4));
-            frame.SetVirtualContainer(VirtualContainerLevel.VC32, 9, new VirtualContainer(VirtualContainerLevel.VC32));
-            frame.SetVirtualContainer(VirtualContainerLevel.VC21, 82, new VirtualContainer(VirtualContainerLevel.VC21));
-            frame.SetVirtualContainer(VirtualContainerLevel.VC12, 250, new VirtualContainer(VirtualContainerLevel.VC12));
-
-
-            FrameBuilder fmb = new SDHFrame.FrameBuilder();
-            string var = fmb.BuildLiteral(frame);
-            
-            frame = (SDHFrame.Frame)fmb.BuildFrame(var);
-
-            Console.WriteLine(frame.ToString());
-
-            String id = "0";
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Input parameter missing");
-            }
-            NetworkNodeSetupProcess setUpProcess = new NetworkNodeSetupProcess("nodeConfig" + id);
-            NetworkNode node = setUpProcess.startNodeProcess();
-            ForwardingRecord record = new ForwardingRecord(4000, 6000, StmLevel.STM1, VirtualContainerLevel.VC32, 0, 1);
-            List<ForwardingRecord> records = new List<ForwardingRecord>();
-            records.Add(record);
-            node.AddForwardingRecords(records);
-            Console.WriteLine("Start emulation");
-            //node.emulateManagement("sub-conection-HPC|1002-2003#|");
-
-        }
-    }
-}
->>>>>>> origin/dev
--- NetworkNode/TTF/MultiplexSectionTermination.cs
+++ NetworkNode/TTF/MultiplexSectionTermination.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using NetworkNode.SDHFrame;
 using System;
 using System.Collections.Generic;
@@ -32,41 +31,3 @@ namespace NetworkNode.TTF
         }
     }
 }
-=======
-﻿using NetworkNode.SDHFrame;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NetworkNode.TTF
-{
-    class MultiplexSectionTermination : SectionTerminator
-    {
-        public void evaluateHeader(IFrame sdhFrame)
-        {
-            if (sdhFrame.Msoh !=null && sdhFrame.Msoh.Checksum == BinaryInterleavedParity.generateBIP(sdhFrame, 8))
-            {
-            }
-            else { }
-        }
-
-        /// <summary>
-        /// Generates the MSOH header.
-        /// </summary>
-        /// <param name="sdhFrame">The SDH frame.</param>
-        public void generateHeader(ref IFrame sdhFrame)
-        {
-            if (sdhFrame.Msoh == null)
-            {
-                return;
-            }
-            SDHFrame.Frame tempFrame = (SDHFrame.Frame)sdhFrame;          
-            ((SDHFrame.Frame)sdhFrame).Msoh.Checksum = BinaryInterleavedParity.generateBIP(tempFrame, 8);
-        }
-    }
-}
->>>>>>> origin/dev
