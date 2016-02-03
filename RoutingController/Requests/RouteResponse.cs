25e0f25f3126dd3fc5fcadf117ee3f74342b6c62
Jan Sobczak <jasobeczek@gmail.com>
2016-02-03 20:13:47 +0100

    QueryRequest pole ID

--- RoutingController/Requests/RouteResponse.cs
+++ RoutingController/Requests/RouteResponse.cs
@@ -24,6 +24,7 @@ namespace RoutingController.Requests
     public class RouteResponse
     {
         public string Protocol { get; private set; }
+        public string Id { get; set; }
         public List<Ends> Ends { get; set; }
         public List<SNP> Steps { get; set; }
 
