2e1b00deb27e31ca2ef906e08a3811ed4a6c2dc0
Jan Sobczak <jasobeczek@gmail.com>
2016-02-05 03:26:00 +0100

    Do zobaczenia co nie działa

---
+++ Configs/start_domena.bat
@@ -0,0 +1,37 @@
+cd WireCloud
+start cmd /c cloudStart.bat
+
+
+cd ..
+cd RoutingController
+start cmd /c routingControllerStart.bat
+
+cd ..
+cd Directory
+start cmd /c startDirectory0.bat
+
+cd ..
+cd Policy
+start cmd /c startPolicy0.bat
+
+cd ..
+cd NCC
+start cmd /c startNCC0.bat
+
+cd ..
+cd LRM
+start cmd /c startLRM0.bat
+
+cd ..
+cd CC
+start cmd /c startConnectionController0.bat
+
+cd ..
+cd NetworkNode
+start cmd /c startNodes_domainA.bat
+
+cd ..
+cd NetworkClient
+start cmd /c startClient1.bat
+start cmd /c startClient2.bat
+exit
